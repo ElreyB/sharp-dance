c6f8e966ec3899a5a14b5a59add8944f0cfbd6d5
Oscar Bartra <obartra@gmail.com>
2019-06-15 10:02:17 -0700

    Wrap Formik elements in Gymnast Grid

---
+++ src/styledGuide/Button/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { useGrid, Grid } from "gymnast";
+import styled from "styled-components/macro";
+
+const StyledButton = styled("button")`
+  border: 1px solid white;
+`;
+
+export const Button = ({
+  size = "fit",
+  padding = "S M",
+  children,
+  role = "button",
+  ...props
+}) => {
+  const [, allProps] = useGrid(props);
+
+  return (
+    <Grid size={size} padding={padding}>
+      <StyledButton {...allProps}>{children}</StyledButton>
+    </Grid>
+  );
+};
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  role: PropTypes.oneOf(["button", "submit", "reset"])
+};
---
+++ src/styledGuide/internal/withGrid.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useGrid } from "gymnast";
+
+export const withGrid = Component => ({ children, ...props }) => {
+  const [showComponent, allProps] = useGrid(props);
+
+  if (showComponent) {
+    return <Component {...allProps}>{children}</Component>;
+  }
+
+  return null;
+};
