50d09c0046708945ae4ff9bbcf2c8756fe8d6c48
obartra <obartra@indeed.com>
2019-06-26 19:21:32 -0700

    feat(pages): Add classes page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import About from "./pages/About";
 import Admin from "./pages/Admin";
 import Bios from "./pages/Bios";
-import Events from "./pages/Events";
-import Login from "./pages/Login";
-import Home from "./pages/Home";
-import SignUp from "./pages/SignUp";
+import Classes from "./pages/Classes";
 import Error404 from "./pages/404";
+import Events from "./pages/Events";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
 import { withAuthentication } from "../src/fbconfig";
 import {
   ABOUT,
   ADMIN,
   BIOS,
+  CLASSES,
   EVENTS,
   LANDING,
   LOG_IN,
@@ -22,14 +24,17 @@ import {
 function App() {
   return (
     <Router>
-      <Route path={ABOUT} component={About} />
-      <Route path={BIOS} exact component={Bios} />
-      <Route path={EVENTS} component={Events} />
-      <Route path={LOG_IN} component={Login} />
-      <Route path={SIGN_UP} component={SignUp} />
-      <Route path={ADMIN} component={Admin} />
-      <Route path={LANDING} exact component={Home} />
-      <Route path={LANDING} component={Error404} />
+      <Switch>
+        <Route path={ABOUT} component={About} />
+        <Route path={BIOS} exact component={Bios} />
+        <Route path={CLASSES} exact component={Classes} />
+        <Route path={EVENTS} component={Events} />
+        <Route path={LOG_IN} component={Login} />
+        <Route path={SIGN_UP} component={SignUp} />
+        <Route path={ADMIN} component={Admin} />
+        <Route path={LANDING} exact component={Home} />
+        <Route component={Error404} />
+      </Switch>
     </Router>
   );
 }
---
+++ src/styledGuide/IFrame/index.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Grid } from "gymnast";
+import styled from "styled-components";
+
+const StyledIframe = styled("iframe")`
+  border: 0;
+  width: 100%;
+  height: ${props => props.height}px;
+  position: relative;
+`;
+
+export function IFrame({
+  size,
+  margin,
+  padding,
+  src,
+  title,
+  height,
+  ...props
+}) {
+  return (
+    <Grid {...{ margin, padding, size }} {...props}>
+      <StyledIframe
+        src={src}
+        title={title}
+        height={height}
+        frameborder="0"
+        allowfullscreen
+      ></StyledIframe>
+    </Grid>
+  );
+}
