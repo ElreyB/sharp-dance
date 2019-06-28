4a292e6f90694d076027738bc5f46af37b28bf79
Elrey Belmonti <elrey.belmonti@formidable.com>
2019-06-28 16:00:31 -0700

    testing reading from database

--- src/index.js
+++ src/index.js
@@ -3,14 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ThemeProvider } from "styled-components/macro";
 import { theme, GlobalStyle } from "./styles";
+import { withDatabase } from "./fbconfig";
 
-const RootApp = () => (
+const RootApp = withDatabase(() => (
   <ThemeProvider theme={theme}>
     <>
       <GlobalStyle />
       <App />
     </>
   </ThemeProvider>
-);
+));
 
 ReactDOM.render(<RootApp />, document.getElementById("root"));
