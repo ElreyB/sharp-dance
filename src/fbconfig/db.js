bb85725018e02fa602a3fb5c19b6d46f6c4f75a4
Elrey Belmonti <elrey.belmonti@formidable.com>
2019-06-27 18:39:16 -0700

    add getDB function

--- src/fbconfig/db.js
+++ src/fbconfig/db.js
@@ -1,22 +1,11 @@
-import { useState, useEffect } from "react";
 import { db } from "./fb";
 
-// User API
-export const AddUser = (id: string, username: string, email: string) =>
-  db.ref(`users/${id}`).set({
-    email,
-    username
-  });
-
-export const onceGetUsers = () => db.ref("staff").once("value");
-export function useDB(ref = undefined) {
-  const [dataBase, setDataBase] = useState();
-  useEffect(() => {
-    db.ref(ref)
-      .once("value")
-      .then(snapshot => {
-        setDataBase(snapshot.value);
-      });
-    return dataBase;
-  }, [ref, dataBase]);
+export function getDB(ref = undefined) {
+  const dataBase = db
+    .ref(ref)
+    .once("value")
+    .then(snapshot => {
+      return snapshot.val();
+    });
+  return dataBase;
 }
--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,8 +1,7 @@
 import React from "react";
 import { Page, H2 } from "../../styledGuide";
-import { useDB } from "../../fbconfig";
+
 export default function About() {
-  console.warn(useDB("staff"));
   return (
     <Page>
       <H2>About</H2>
