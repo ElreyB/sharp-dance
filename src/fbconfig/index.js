e66692b195b76c98f3d911bb267d31317ca14469
Elrey Belmonti <elrey.belmonti@formidable.com>
2019-07-11 19:12:41 -0700

    clean up

--- src/fbconfig/index.js
+++ src/fbconfig/index.js
@@ -2,5 +2,3 @@ export * from "./fb";
 export * from "./auth";
 export * from "./db";
 export * from "./AuthUserContext";
-export * from "./withDatabase";
-export * from "./DatabaseContext";
--- src/index.js
+++ src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { ThemeProvider } from "styled-components/macro";
 import { theme, GlobalStyle } from "./styles";
-import { withDatabase, AuthUserProvider } from "./fbconfig";
+import { AuthUserProvider } from "./fbconfig";
 
-const RootApp = withDatabase(() => (
+const RootApp = () => (
   <ThemeProvider theme={theme}>
     <AuthUserProvider>
       <GlobalStyle />
       <App />
     </AuthUserProvider>
   </ThemeProvider>
-));
+);
 
 ReactDOM.render(<RootApp />, document.getElementById("root"));
