85b649f66bfa1f26467e6d7fc279085469cc4700
obartra <obartra@indeed.com>
2019-09-24 19:24:03 -0700

    refactor(homepage): move logic to homepage

--- src/pages/About/index.js
+++ src/pages/About/index.js
@@ -1,22 +1,9 @@
 import React, { useContext } from "react";
-import {
-  Grid,
-  H2,
-  Banner,
-  Markdown,
-  Img,
-  Page,
-  Quote
-} from "../../styledGuide";
+import { Grid, H2, Banner, Markdown, Img, Page } from "../../styledGuide";
 import Loading from "../Loading";
-import {
-  QuotesContext,
-  OrganizationsContext,
-  PagesContext
-} from "../../Providers";
+import { OrganizationsContext, PagesContext } from "../../Providers";
 
 export default function About() {
-  const quotes = useContext(QuotesContext);
   const organizations = useContext(OrganizationsContext);
   const { getPage } = useContext(PagesContext);
 
@@ -32,32 +19,22 @@ export default function About() {
     <Page>
       <Banner {...headerBanner} />
       <Grid align="start">
-        {quotes && (
-          <Grid size={3} show="desktop" marginRight="XL">
-            <H2>{options.quotesTitle}</H2>
-            {quotes.map((quote, i) => (
-              <Quote {...quote} key={i} />
+        <Markdown>{options.content}</Markdown>
+        {organizations && (
+          <Grid justify="center">
+            <H2>Members</H2>
+            {organizations.map(org => (
+              <Img
+                size="fit"
+                margin="M"
+                align="center"
+                alt={org.organization}
+                src={org.logo}
+                key={org.organization}
+              />
             ))}
           </Grid>
         )}
-        <Grid size="auto">
-          <Markdown>{options.content}</Markdown>
-          {organizations && (
-            <Grid justify="center">
-              <H2>Members</H2>
-              {organizations.map(org => (
-                <Img
-                  size="fit"
-                  margin="M"
-                  align="center"
-                  alt={org.organization}
-                  src={org.logo}
-                  key={org.organization}
-                />
-              ))}
-            </Grid>
-          )}
-        </Grid>
       </Grid>
     </Page>
   );
