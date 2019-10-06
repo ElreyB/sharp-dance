3ca39e11366478e59a058e232e2b250bc25657be
Elrey Belmonti <elrey.belmonti@formidable.com>
2019-10-06 11:49:46 -0700

    remove content under homepage video

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,24 +1,7 @@
 import React from "react";
-
 import { FullPageVideo, Grid, Markdown, Page, Quote } from "../../styledGuide";
 import { PagesContext, QuotesContext } from "../../Providers";
 import Loading from "../Loading";
-
-function getRandomQuoteIndex(length) {
-  if (length === 0) {
-    return [-1, -1];
-  }
-  if (length === 1) {
-    return [0, -1];
-  }
-  if (length === 2) {
-    return [0, 1];
-  }
-  const first = Math.round(Math.random() * (length - 1));
-  const next = (first + 1) % length;
-
-  return [first, next];
-}
 
 export default function Home() {
   const { getPage } = React.useContext(PagesContext);
@@ -30,23 +13,10 @@ export default function Home() {
   }
 
   const { options = {} } = page;
-  const [firstQuoteIndex, secondQuoteIndex] = getRandomQuoteIndex(
-    quotes.length
-  );
 
   return (
     <Page>
       <FullPageVideo src={options.video} />
-      <Markdown marginTop="XL">{options.content}</Markdown>
-      <Grid marginTop="XL">
-        <Quote {...quotes[firstQuoteIndex]} size={{ desktop: 6, mobile: 12 }} />
-        <Quote
-          {...quotes[secondQuoteIndex]}
-          marginLeft="2XL"
-          show="desktop"
-          size={6}
-        />
-      </Grid>
     </Page>
   );
 }
