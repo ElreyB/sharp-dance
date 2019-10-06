900de8e4de29b088a22ca13382d5287c3effd5cb
Elrey Belmonti <elbelmonti@gmail.com>
2019-10-06 12:47:13 -0700

    remove unuesd components

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,11 +1,10 @@
 import React from "react";
-import { FullPageVideo, Grid, Markdown, Page, Quote } from "../../styledGuide";
-import { PagesContext, QuotesContext } from "../../Providers";
+import { FullPageVideo Page } from "../../styledGuide";
+import { PagesContext} from "../../Providers";
 import Loading from "../Loading";
 
 export default function Home() {
   const { getPage } = React.useContext(PagesContext);
-  const quotes = React.useContext(QuotesContext);
   const page = getPage("home");
 
   if (!page) {
