3020a08189b8b400c22e3067e74cbd2b0ac9589a
Elrey Belmonti <elbelmonti@gmail.com>
2019-10-06 19:48:52 -0700

    refactor after talking with oscar

--- src/pages/Loading/index.js
+++ src/pages/Loading/index.js
@@ -61,16 +61,15 @@ const Loading = () => {
   return (
     <Container>
       <Logo>SHARP DANCE CO.</Logo>
-
       <svg
         viewBox="0 0 1024 1024"
         version="1.1"
         xmlns="http://www.w3.org/2000/svg"
-        // xmlns:xlink="http://www.w3.org/1999/xlink"
+        xmlnsXlink="http://www.w3.org/1999/xlink"
       >
-        <circle id="Oval" cx="512" cy="512" r="512" />
-        <circle id="Oval" cx="512" cy="512" r="512" />
-        <circle id="Oval" cx="512" cy="512" r="512" />
+        <circle cx="512" cy="512" r="512" />
+        <circle cx="512" cy="512" r="512" />
+        <circle cx="512" cy="512" r="512" />
       </svg>
     </Container>
   );
