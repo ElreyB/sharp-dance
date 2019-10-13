2609d770e8b1c75eb2951a40dd2f3d95d2243d72
obartra <obartra@indeed.com>
2019-10-13 14:55:39 -0700

    chore(npm): update dependencies

--- src/styledGuide/Img/Img.js
+++ src/styledGuide/Img/Img.js
@@ -1,26 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Grid } from "gymnast";
-import styled, { css } from "styled-components/macro";
+import styled from "styled-components/macro";
 import { Credit } from "../internal/Credit";
 
 const ImgWrapper = styled(Grid)`
   position: relative;
-  ${({ media }) =>
-    media &&
-    css`
-      height: 300px;
-    `}
 `;
 
 const ImgGrid = styled(Grid)`
   background-repeat: no-repeat;
   background-position: center;
-  ${({ media }) =>
-    media &&
-    css`
-      height: 100%;
-    `}
 `;
 
 const InnerImg = styled("img")`
@@ -34,7 +24,6 @@ export const Img = ({
   alt,
   credit,
   backgroundSize = "cover",
-  media = false,
   ...props
 }) => {
   const role = alt ? undefined : "presentation";
@@ -42,12 +31,11 @@ export const Img = ({
   const combinedAlt = alt || (src && src.title) ? src.title : undefined;
 
   return (
-    <ImgWrapper margin="0 M/2" media={media} {...props}>
+    <ImgWrapper margin="0 M/2" {...props}>
       <ImgGrid
         align="center"
         justify="center"
         style={{ backgroundImage: `url("${srcString}")`, backgroundSize }}
-        media={media}
       >
         <InnerImg src={srcString} alt={combinedAlt} role={role} />
       </ImgGrid>
