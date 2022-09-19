222732f45c70e46156633507e537c48eb406a518
Elrey Belmonti <elreybelmonti@gmail.com>
2022-09-19 10:56:38 -0400

    comment out socail icons

--- src/styledGuide/Footer/index.js
+++ src/styledGuide/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components/macro";
-import { SocialIcons } from "../SocialIcons";
+// import { SocialIcons } from "../SocialIcons";
 import { Mailchimp } from "../Mailchimp";
 import { A } from "../A";
 import {
