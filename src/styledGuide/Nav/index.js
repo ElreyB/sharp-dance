a083d0b8dbdc3049dc031d4c8e0ae5680ae91f68
Elrey Belmonti <elreybelmonti@gmail.com>
2022-11-27 06:53:51 -0500

    board feedback

--- src/styledGuide/Nav/index.js
+++ src/styledGuide/Nav/index.js
@@ -15,7 +15,7 @@ import { Ul } from "./Ul";
 
 const links = [
   { to: DIANE, label: "Diane Sharp-Nachsin" },
-  { to: BIOS, label: "SHARP Family" },
+  { to: BIOS, label: "Meet the SHARP Family" },
   { to: MEDIA, label: "Repertoire" },
   // TODO: better way to show or remove link
   { to: INTENSIVE, label: "Intensive" },
